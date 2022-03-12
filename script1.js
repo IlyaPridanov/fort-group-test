2d848479938a405971e147e415a52f7d9cbcac68
ilyapridanov <pkukudmitriykuku@gmail.com>
2022-03-12 21:40:47 +0300

    feat: functions stage 1

--- script1.js
+++ script1.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 const DATA_ERROR = {
   "cardArr": [
     {
@@ -35,56 +34,153 @@ const DATA_ERROR = {
   ]
 }
 
-const build = (data) => {
-  const body = document.querySelector('body');
-  
-  const selectItem = () => {
-    let result = '';
-    data.cardArr.forEach(element => {
-      result += `<li class="select__item" data-card="${element.dataName}">${element.caption}</li>`;
-    });
-    return result;
-  }
-  
-  const cardItem = () => {
-    let result = '';
-    data.cardArr.forEach(element => {
-      result += `
-        <li class="block__item card" data-card="${element.dataName}">
-          <div class="card__img">
-            <img src="${element.src}" alt="card-img">
+// Какие карточки показанны изначально
+
+let cardFlag = {
+  "1": true,
+  "2": true,
+  "3": true,
+  "4": true,
+  "5": true,
+  "6": false,
+  "7": false,
+}
+
+const startBuild = (dataStart) => {
+  // Построение
+  const root = document.querySelector('#root');
+
+  const build = (data) => {
+    const selectItem = () => {
+      let result = '';
+      data.cardArr.forEach(element => {
+        if (!cardFlag[element.dataName]) {
+          result += `<li class="select__item" data-card="${element.dataName}">${element.caption}</li>`;
+        }
+      });
+      return result;
+    }
+    
+    const cardItem = () => {
+      let result = '';
+      data.cardArr.forEach(element => {
+        if (cardFlag[element.dataName]) {
+          result += `
+            <li class="block__item card" data-card="${element.dataName}">
+              <div class="card__img">
+                <img src="${element.src}" alt="card-img">
+              </div>
+              <div class="card__caption">${element.caption}</div>
+              <button class="card__close">x</button>
+            </li>
+          `;
+        }
+      });
+      return result;
+    }
+    
+    const layout = () => (
+      `
+      <div class="block">
+        <div class="block__column">
+          <div class="block__select select">
+            <div class="select__caption">Добавить карточку</div>
+            <ul class="select__list">
+            ${selectItem()}
+            </ul>
           </div>
-          <div class="card__caption">${element.caption}</div>
-          <button class="card__close">x</button>
-        </li>
-      `;
+        </div>
+        <ul class="block__list">
+          ${cardItem()}
+        </ul>
+      </div>
+      <div class="popup">Произошло удаление...</div>
+      `
+    );
+    
+    root.insertAdjacentHTML('afterbegin', layout());
+  }
+
+  build(dataStart);
+
+  // Показ попапа
+
+  const getPopup = () => {
+    const popup = document.querySelector(".popup");
+    popup.classList.add('active');
+
+    setTimeout(() => {
+      popup.classList.remove('active');
+    }, 700);
+  }
+
+  let cardCloseBtn = document.querySelectorAll(".card__close");
+  let selectItem = document.querySelectorAll(".select__item");
+
+  // Удаление карточки
+
+  const closeCard = function () {
+    let element = this;
+    let numberCard = element.parentElement.dataset.card;
+    cardCloseBtn.forEach(element => {
+      element.removeEventListener('click', closeCard);
     });
-    return result;
+    selectItem.forEach(element => {
+      element.removeEventListener('click', closeCard);
+    });
+    cardFlag[numberCard] = !cardFlag[numberCard];
+    while (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+    build(dataStart);
+    getPopup();
+    cardCloseBtn = document.querySelectorAll(".card__close");
+    selectItem = document.querySelectorAll(".select__item");
+    cardListener();
+    selectListener();
+  }
+
+  function cardListener () {
+    cardCloseBtn.forEach(element => {
+      element.addEventListener('click', closeCard);
+    });
+  }
+
+  cardListener();
+
+  // Добавление карточки
+
+  const openCard = function () {
+    let element = this;
+    let numberCard = element.dataset.card;
+    selectItem.forEach(element => {
+      element.removeEventListener('click', closeCard);
+    });
+    cardCloseBtn.forEach(element => {
+      element.removeEventListener('click', closeCard);
+    });
+    cardFlag[numberCard] = !cardFlag[numberCard];
+    while (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+    build(dataStart);
+    cardCloseBtn = document.querySelectorAll(".card__close");
+    selectItem = document.querySelectorAll(".select__item");
+    selectListener();
+    cardListener();
   }
   
-  const layout = () => (
-    `
-    <div class="block">
-      <div class="block__column">
-        <div class="block__select select">
-          <div class="select__caption">Добавить карточку</div>
-          <ul class="select__list">
-          ${selectItem()}
-          </ul>
-        </div>
-      </div>
-      <ul class="block__list">
-        ${cardItem()}
-      </ul>
-    </div>
-    <div class="popup">Произошло удаление...</div>
-    `
-  );
-  
-  body.insertAdjacentHTML('afterbegin', layout());
+  function selectListener () {
+    selectItem.forEach(element => {
+      element.addEventListener('click', openCard);
+    });
+  }
+
+  selectListener();
 }
 
 fetch("./data.json")
   .then(response => response.json())
-  .then(json => build(json))
-  .catch(() => build(DATA_ERROR));
+  .then(json => startBuild(json))
+  .catch(() => startBuild(DATA_ERROR));
+
